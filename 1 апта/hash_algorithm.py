c93fb2c30bc5dec758663f0fb1354223459ff842
nazgab <gabdolnazym@gmail.com>
2025-01-30 18:26:49 +0500

    hash_algorithm.py

--- 1 апта/hash_algorithm.py
+++ 1 апта/hash_algorithm.py
@@ -2,9 +2,12 @@ def simple_hash(data):
     hash_value = 0
     for char in data:
         hash_value += ord(char)
-        hash_value *= 31  # Бір қарапайым көбейту
-        hash_value %= 1000000  # Шектеулі диапазон
-    return hash_value
+        hash_value *= 31  # Простое умножение
+        hash_value %= 1000000  # Ограничение диапазона
+
+    return f"{hash_value:06d}"  # Всегда 6 знаков, дополняем нулями
+
 
 if __name__ == "__main__":
-    print("Хэш:", simple_hash("Block Data"))
+    print("Хэш:", simple_hash("54"))
+    print("Хэш:", simple_hash("Block"))
