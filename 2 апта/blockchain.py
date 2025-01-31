09288b83635051b0554d8e6a4f83edd7c00c956d
kirmizibimagambet <kirmizibimagambet@gmail.com>
2025-01-31 21:42:47 +0500

    blockchain.py

--- 2 апта/blockchain.py
+++ 2 апта/blockchain.py
@@ -1,28 +1,21 @@
+from block import Block
+from utxo import UTXO
+
 class Blockchain:
     def __init__(self):
         self.chain = [self.create_genesis_block()]
         self.utxo = UTXO()
 
     def create_genesis_block(self):
+        """Генезис блокты құру"""
         return Block(0, "0", [])
 
     def add_block(self, transactions):
-        valid_transactions = []
-        for tx in transactions:
-            if self.utxo.process_transaction(tx) and self.is_valid_merkle_root(tx):
-                valid_transactions.append(tx)
-
-        # Егер жарамсыз транзакциялар болмаса, блокты қосамыз
-        if valid_transactions:
-            previous_block = self.chain[-1]
-            new_block = Block(len(self.chain), previous_block.hash, valid_transactions)
-            self.chain.append(new_block)
-
-    def is_valid_merkle_root(self, block):
-        """Меркле түбірінің дұрыстығын тексеру"""
-        tx_hashes = [tx.tx_hash for tx in block.transactions]
-        merkle_tree = MerkleTree(tx_hashes)
-        return merkle_tree.root == block.merkle_root
+        """Жаңа блок қосу"""
+        valid_transactions = [tx for tx in transactions if self.utxo.process_transaction(tx)]
+        previous_block = self.chain[-1]
+        new_block = Block(len(self.chain), previous_block.hash, valid_transactions)
+        self.chain.append(new_block)
 
     def is_chain_valid(self):
         """Блокчейннің дұрыстығын тексеру"""
@@ -30,9 +23,11 @@ class Blockchain:
             current_block = self.chain[i]
             previous_block = self.chain[i - 1]
 
+ # Блоктың хэшін тексеру
             if current_block.hash != current_block.calculate_hash():
                 return False
 
+            # Алдыңғы блоктың хэшін тексеру
             if current_block.previous_hash != previous_block.hash:
                 return False
 
@@ -40,9 +35,14 @@ class Blockchain:
             if not self.is_valid_merkle_root(current_block):
                 return False
 
-            # Баланстың теріс болмауы
+            # Баланстың теріс болмауын тексеру
             for tx in current_block.transactions:
                 if not self.utxo.process_transaction(tx):
-                    return False
+                    return False 
+            if current_block.hash != current_block.calculate_hash():
+                return False
+
+            if current_block.previous_hash != previous_block.hash:
+                return False
 
         return True
