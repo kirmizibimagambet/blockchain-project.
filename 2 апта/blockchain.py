795cb03d0da51eb00ba6a9e88cd176814cfd8e93
kirmizibimagambet <kirmizibimagambet@gmail.com>
2025-01-31 21:01:52 +0500

    Update blockchain.py

--- 2 апта/blockchain.py
+++ 2 апта/blockchain.py
@@ -1,21 +1,28 @@
-from block import Block
-from utxo import UTXO
-
 class Blockchain:
     def __init__(self):
         self.chain = [self.create_genesis_block()]
         self.utxo = UTXO()
 
     def create_genesis_block(self):
-        """Генезис блокты құру"""
         return Block(0, "0", [])
 
     def add_block(self, transactions):
-        """Жаңа блок қосу"""
-        valid_transactions = [tx for tx in transactions if self.utxo.process_transaction(tx)]
-        previous_block = self.chain[-1]
-        new_block = Block(len(self.chain), previous_block.hash, valid_transactions)
-        self.chain.append(new_block)
+        valid_transactions = []
+        for tx in transactions:
+            if self.utxo.process_transaction(tx) and self.is_valid_merkle_root(tx):
+                valid_transactions.append(tx)
+
+        # Егер жарамсыз транзакциялар болмаса, блокты қосамыз
+        if valid_transactions:
+            previous_block = self.chain[-1]
+            new_block = Block(len(self.chain), previous_block.hash, valid_transactions)
+            self.chain.append(new_block)
+
+    def is_valid_merkle_root(self, block):
+        """Меркле түбірінің дұрыстығын тексеру"""
+        tx_hashes = [tx.tx_hash for tx in block.transactions]
+        merkle_tree = MerkleTree(tx_hashes)
+        return merkle_tree.root == block.merkle_root
 
     def is_chain_valid(self):
         """Блокчейннің дұрыстығын тексеру"""
@@ -28,5 +35,14 @@ class Blockchain:
 
             if current_block.previous_hash != previous_block.hash:
                 return False
+
+            # Меркле түбірін тексеру
+            if not self.is_valid_merkle_root(current_block):
+                return False
+
+            # Баланстың теріс болмауы
+            for tx in current_block.transactions:
+                if not self.utxo.process_transaction(tx):
+                    return False
 
         return True
