d989a844323097396ab1351616252e4b349bb010
nazgab <gabdolnazym@gmail.com>
2025-02-20 22:21:50 +0500

    transaction.py

--- transaction.py
+++ transaction.py
@@ -20,15 +20,17 @@ class Transaction:
 
     def sign_transaction(self, private_key):
         """ Транзакцияны жеке кілт арқылы қол қою """
+        if not isinstance(private_key, rsa.PrivateKey):  # Тек PrivateKey қабылдайды
+            raise TypeError(f"Invalid private key format: {type(private_key)}. Expected rsa.PrivateKey.")
         return rsa.sign(self.tx_hash.encode(), private_key, 'SHA-256')
 
     def verify_signature(self, public_key):
         """ Қолтаңбаны ашық кілт арқылы тексеру """
         try:
-            if isinstance(public_key, str):  # Егер public_key string болса, оны қайта жүктеу керек
-                public_key = rsa.PublicKey.load_pkcs1(public_key.encode())
+            if isinstance(public_key, str):  # Егер public_key жол болса, оны rsa.PublicKey-ке айналдыру
+                public_key = rsa.PublicKey.load_pkcs1(public_key.encode('utf-8'))
             
-            rsa.verify(self.tx_hash.encode(), self.signature, public_key)
+            rsa.verify(self.tx_hash.encode('utf-8'), self.signature, public_key)
             return True
         except rsa.VerificationError:
             return False
